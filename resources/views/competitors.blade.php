4b2ddbe93c993956ac82cd91c2f90bb4b4754766
alessandroordetti <alessandro.ordetti@gmail.com>
2024-08-08 12:19:18 +0200

    Displaying simple layout

--- resources/views/competitors.blade.php
+++ resources/views/competitors.blade.php
@@ -1,4 +1,29 @@
-<?php
+@extends('layout.main')
 
-
-dd($competitors);
+@section('content')
+<h1>Products</h1>
+<div class="col-12">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Competitor</th>
+                <th scope="col">SKU</th>
+                <th scope="col">Product</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($competitors as $competitor)
+            <tr>
+                <th scope="row">{{$competitor->id}}</th>
+                <td>{{$competitor->competitor}}</td>
+                <td>{{$competitor->sku}}</td>
+                <td>{{$competitor->product_title}}</td>
+                <td>{{$competitor->sale_price}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
